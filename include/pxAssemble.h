9b758c729817ee16bf96ca2a94cd53d6c6d470c7
iivlx <iivlx@iivlx.com>
2025-02-27 00:19:50 -0800

    dynamically assemble file during runtime...

--- include/pxAssemble.h
+++ include/pxAssemble.h
@@ -1,4 +1,4 @@
 #include <string>
 
 /* Assembles an input string into px machine code... */
-std::pair<uint8_t*, size_t> pxAssemble(std::string code);
+std::pair<uint8_t*, size_t> pxAssemble(std::string input_file, std::string output_file="out/output.pbin");
